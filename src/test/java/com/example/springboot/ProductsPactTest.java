36c0479f8caffb54bbce8380f0eb6241ba677058
Matt Fellows <matt.fellows@onegeek.com.au>
2020-06-03 17:51:42 +1000

    fix: spelling mistake in provider name

--- src/test/java/com/example/springboot/ProductsPactTest.java
+++ src/test/java/com/example/springboot/ProductsPactTest.java
@@ -20,7 +20,7 @@ import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 @ExtendWith(SpringExtension.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
-@Provider(" pactflow-example-provider-springboot")
+@Provider("pactflow-example-provider-springboot")
 @PactBroker(scheme = "https", host = "${PACT_BROKER_BASE_URL}",
   authentication = @PactBrokerAuth(token = "${PACT_BROKER_TOKEN}"))
 class ProductsPactTest {
