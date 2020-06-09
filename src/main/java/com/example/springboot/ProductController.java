78eeb8364ec5489f6d98062502c56775817bfc3d
Matt Fellows <matt.fellows@onegeek.com.au>
2020-06-09 17:55:25 +1000

    chore: add CORS

--- src/main/java/com/example/springboot/ProductController.java
+++ src/main/java/com/example/springboot/ProductController.java
@@ -11,8 +11,10 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 @RestController
+@CrossOrigin(origins = {"*"})
 @RequestMapping(
   value = "/",
   produces = MediaType.APPLICATION_JSON_UTF8_VALUE
--- src/test/java/com/example/springboot/ProductsPactTest.java
+++ src/test/java/com/example/springboot/ProductsPactTest.java
@@ -15,7 +15,6 @@ import org.junit.jupiter.api.TestTemplate;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.context.annotation.Bean;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 @ExtendWith(SpringExtension.class)
