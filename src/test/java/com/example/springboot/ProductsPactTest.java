67be97d6a26c6515319190041278dab68724a147
Yousaf Nabi <yousafn@gmail.com>
2022-04-01 12:51:02 +0100

    ci: use PACT_BROKER_BASE_URL

--- src/test/java/com/example/springboot/ProductsPactTest.java
+++ src/test/java/com/example/springboot/ProductsPactTest.java
@@ -18,7 +18,7 @@ import org.springframework.test.context.junit.jupiter.SpringExtension;
 @ExtendWith(SpringExtension.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
 @Provider("pactflow-example-provider-springboot")
-@PactBroker(scheme = "https", host = "${PACT_BROKER_HOST}", tags = { "master",
+@PactBroker(scheme = "https", host = "${PACT_BROKER_BASE_URL}", tags = { "master",
     "prod" }, providerTags = "master", enablePendingPacts = "true", authentication = @PactBrokerAuth(token = "${PACT_BROKER_TOKEN}"))
 class ProductsPactTest {
 
